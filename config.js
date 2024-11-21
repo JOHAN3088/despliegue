9ec33bac4487129cfd6fcf1bfd0d8183b4a915d4
JOHAN3088 <johan3088@hotmail.com>
2024-11-20 21:52:57 -0500

    despliegue backend

---
+++ config.js
@@ -0,0 +1,8 @@
+const mongoose=require("mongoose");
+require ('dotenv').config()
+const dbconect=()=>{
+    mongoose.set('strictQuery',true);
+    mongoose.connect(process.env.MONGO_URL).then((succes)=> console.log("conexion exitosa")).catch((err)=> console.log(err.message));  
+  }
+
+  module.exports = dbconect;
